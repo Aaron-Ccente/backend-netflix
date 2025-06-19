e6c44b79294b37e7be69b4efc65cf96849dda520
Aaron <146492253+Aaron-Ccente@users.noreply.github.com>
2025-06-18 21:48:41 -0500

    password eliminte

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Ccentejuan06@",
+    password: "",
     database: "signup"
 });
 
